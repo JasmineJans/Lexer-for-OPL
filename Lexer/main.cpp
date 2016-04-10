59c3803896897adffed07ec83dab835b965a6878
Jasmine Jans <jasminejans@jasmines-macbook-pro-2.local>
2016-04-10 14:27:31 -0700

    Initial Commit

---
+++ Lexer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lexer
+//
+//  Created by Jasmine Jans on 4/10/16.
+//  Copyright © 2016 Jasmine Jans. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
